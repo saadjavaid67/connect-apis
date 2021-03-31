03949d2e07f4fd6806270a13decff558cdfdb954
saadjavaid67 <Saadjavaid67@gmail.com>
2021-03-31 19:28:31 +0500

    .

---
+++ app.js
@@ -0,0 +1,42 @@
+const body = document.querySelector('body');
+const button = document.querySelector('button')
+const word = document.createElement('h1');
+
+const randomWord = () => {
+    fetch('https://random-word-api.herokuapp.com/word?number=1').then(response => {
+        return response.json();
+    }).then(
+        response => {
+            word.textContent = response;
+            body.appendChild(word);
+            randomDefinition(response);
+        }
+
+    ).catch(err => {
+        console.log(err);
+    })
+}
+
+const randomDefinition = (w) => {
+    fetch("https://simple-elegant-translation-service.p.rapidapi.com/translate", {
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "x-rapidapi-key": "ed982214b6msh9be77576224f846p179174jsn8c191f12cd24",
+                "x-rapidapi-host": "simple-elegant-translation-service.p.rapidapi.com"
+            },
+            "body": {
+                "text": w
+            }
+        })
+        .then(response => {
+            console.log(response.translated['3.translated text']);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+
+
+button.addEventListener('click', randomWord)
